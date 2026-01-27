926b6c833e7a685620d89336a75992156ac43e8d
Youngwoo Kim <91710667+wooo73@users.noreply.github.com>
2026-01-27 20:29:27 +0900

    [algorithm]: 링크드리스트(2) getNode, addNode, deleteNode

--- algorithm/lecture/lecture-1/section2/linkedlist.ts
+++ algorithm/lecture/lecture-1/section2/linkedlist.ts
@@ -30,6 +30,55 @@ class LinkedList {
       current = current.next;
     }
   }
+
+  /** 원소 찾기 */
+  getNode(index: number) {
+    let currentIndex = 0;
+    let current: Node | null = this.head;
+
+    while (currentIndex < index) {
+      currentIndex += 1;
+      current = current?.next || null;
+    }
+
+    return current || null;
+  }
+
+  /** 원소 추가 */
+  addNode(index: number, data: number) {
+    if (index === 0) {
+      const currHead = this.head;
+      this.head = new Node(data);
+      this.head.next = currHead;
+      return this.head;
+    }
+
+    let prevNode = this.getNode(index - 1);
+    let nextNode = prevNode!.next;
+
+    const newNode = new Node(data);
+
+    prevNode!.next = newNode;
+    newNode!.next = nextNode;
+  }
+
+  /** 원소 삭제 */
+  deleteNode(index: number) {
+    // [1]  [2]  [3]  [4]  [5]
+
+    if (index === 0) {
+      let currHead = this.head;
+      this.head = currHead!.next!;
+      return this.head;
+    }
+
+    let prevNode = this.getNode(index - 1);
+    let targetNode = prevNode!.next;
+    let nextNode = targetNode!.next;
+
+    prevNode!.next = nextNode;
+    targetNode!.next = null;
+  }
 }
 
 const linkedList = new LinkedList(1);
@@ -38,4 +87,23 @@ linkedList.append(3);
 linkedList.append(4);
 linkedList.append(5);
 
+console.log("---------------append----------------");
 linkedList.printAll();
+
+console.log("---------------getNode index 4 찾기----------------");
+console.log(linkedList.getNode(4)?.data);
+
+console.log("---------------addNode index 2 이후 10 추가----------------");
+linkedList.addNode(2, 10);
+linkedList.printAll();
+console.log("---------------addNode index 0 이후 98 추가----------------");
+linkedList.addNode(0, 98);
+linkedList.printAll();
+console.log("--------------------------------\n\n");
+console.log("---------------deleteNode index 2 삭제----------------");
+linkedList.deleteNode(2);
+linkedList.printAll();
+console.log("---------------deleteNode index 0 삭제----------------");
+linkedList.deleteNode(0);
+linkedList.printAll();
+console.log("--------------------------------\n\n");
